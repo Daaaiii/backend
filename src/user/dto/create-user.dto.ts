cbb26c867791ba8326d6e8ce5e599bf5fcce63fe
Daiane Bolzan <dainha85@gmail.com>
2023-09-28 20:58:20 -0300

    FEAT: crud do usuário

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -8,11 +8,7 @@ export class CreateUserDto {
   email: string;
 
   @IsStrongPassword({
-    minLength: 6,
-    minNumbers: 1,
-    minLowercase: 1,
-    minSymbols: 1,
-    minUppercase: 1,
+    minLength: 8,
   })
   password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,42 +1,39 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ParamId } from 'src/decorators/param-id.decorator';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiTags()
   @Post()
   create(@Body() data: CreateUserDto) {
     return this.userService.create(data);
   }
 
+  @ApiTags()
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  @ApiTags()
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@ParamId() id: string) {
     return this.userService.findOne(+id);
   }
-
+  @ApiTags()
   @Patch(':id')
-  update(@Param('id') @Para id: string, @Body() data: UpdateUserDto) {
+  update(@ParamId() id: string, @Body() data: UpdateUserDto) {
     return this.userService.update(+id, data);
   }
-
+  @ApiTags()
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@ParamId() id: string) {
     return this.userService.remove(+id);
   }
 }
