1c9887fd5b3b076c1e1ba8f5f62f3f77edfd2097
Daiane Bolzan <dainha85@gmail.com>
2023-09-29 18:01:14 -0300

    FEAT: adiciona proteção contra ataque de força bruta

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module, forwardRef } from '@nestjs/common';
-
+import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+    ]),
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
