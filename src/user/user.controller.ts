039215937e2f3e9d3a7f731450181bcb3bfcedcb
Daiane Bolzan <dainha85@gmail.com>
2023-09-28 21:37:56 -0300

    FIX: altera commit anterior errado

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,31 +10,34 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ParamId } from 'src/decorators/param-id.decorator';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiTags()
   @Post()
   create(@Body() data: CreateUserDto) {
     return this.userService.create(data);
   }
-
+  @ApiTags()
   @Get()
   findAll() {
     return this.userService.findAll();
   }
-
+  @ApiTags()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
-
+  @ApiTags()
   @Patch(':id')
-  update(@Param('id') @Para id: string, @Body() data: UpdateUserDto) {
+  update( @ParamId() id: string, @Body() data: UpdateUserDto) {
     return this.userService.update(+id, data);
   }
-
+  @ApiTags()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,66 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  create({ name, email, password }: CreateUserDto) {
+    return this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+        createdat: new Date(),
+        updateat: new Date(),
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    return this.prisma.user.findMany({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    await this.exists(id);
+    return this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, { name, email, password }: UpdateUserDto) {
+    this.exists(id);
+
+    return await this.prisma.user.update({
+      where: { id },
+      data: {
+        name,
+        email,
+        password,
+        updateat: new Date(),
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
+
+  async exists(id: number) {
+    if (
+      !(await this.prisma.user.count({
+        where: {
+          id,
+        },
+      }))
+    ) {
+      // throw new NotFoundException(`O usuário ${id} não existe.`);
+    }
   }
 }
